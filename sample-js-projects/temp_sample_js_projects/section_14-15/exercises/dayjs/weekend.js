cd12e22ebed01430348826c238818a9d4c46a265
sunilraj <touch.sunilraj@gmail.com>
2025-10-25 18:19:58 +0100

    Add folder with all subfolders and files

--- sample-js-projects/temp_sample_js_projects/section_14-15/exercises/dayjs/weekend.js
+++
@@ -1,5 +0,0 @@
-export function isWeekend(day) {
-    return (day.format('dddd') === 'Saturday' || day.format('dddd') === 'Sunday');
-}
-
-export default isWeekend;
--- sample-js-projects/temp_sample_js_projects/temp_sample-js-projects/section_16/scripts/checkout.js
+++
@@ -1,5 +0,0 @@
-import { renderOrderSummary } from "./checkout/order-summary.js";
-import { renderPaymentSummary } from "./checkout/payments-summary.js";
-
-renderOrderSummary();
-renderPaymentSummary();
